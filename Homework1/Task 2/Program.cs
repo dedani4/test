a1e56120829e601fd353d2dc07ee8ad630ef6957
dedani4 <dedani4@gmail.com>
2022-12-18 15:04:25 +0300

    add Task 2 solution

--- Homework1/Task 2/Program.cs
+++ Homework1/Task 2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее.*/
+
+using static System.Console;
+
+Write("Enter two numbers: ");
+int a = Convert.ToInt32(ReadLine());
+int b = Convert.ToInt32(ReadLine());
+
+if (a>b) WriteLine($"max = {a}");
+if (a<b) WriteLine($"max = {b}");
+    else WriteLine ("numbers are equal");
+
