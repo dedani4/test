9dfe04ad937b5681329c74c5d4a147af5b65598c
dedani4 <dedani4@gmail.com>
2022-12-18 15:26:56 +0300

    add Task 4 solution

--- Homework1/Task 4/Program.cs
+++ Homework1/Task 4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход три числа 
+и выдаёт максимальное из этих чисел.*/
+
+using static System.Console;
+
+Clear();
+
+Write("Enter first number: ");
+int a = Convert.ToInt32(ReadLine());
+Write("Enter second number: ");
+int b = Convert.ToInt32(ReadLine());
+Write("Enter third number: ");
+int c = Convert.ToInt32(ReadLine());
+
+if(a>b){
+    if(a>c || a==c) Write($"max = {a}");
+        else Write($"max = {c}");
+}
+if(a<b){
+    if(b>c || b==c) Write($"max = {b}");
+        else Write($"max = {c}");
+}
+if(a==b && b==c) WriteLine("All numbers are equal");
