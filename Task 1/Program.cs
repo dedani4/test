fac582bf3a08d7a8caa12b16dd8b89637e3c83a9
dedani4 <dedani4@gmail.com>
2022-12-18 13:14:42 +0300

    add Task 1 solution

---
+++ Task 1/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+ WriteLine ("Enter 2 numbers: ");
+ int a = Convert.ToInt32(ReadLine());
+ int b = Convert.ToInt32(ReadLine());
+
+ if ( a == b*b ){
+    WriteLine($"Number {a} is number {b} squared");
+ }
+ else 
+ WriteLine ($"Number {a} is not squared number {b}");
