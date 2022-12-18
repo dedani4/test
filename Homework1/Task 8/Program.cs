87fb4622bcbfff3862154ea81b91161e65d91c49
dedani4 <dedani4@gmail.com>
2022-12-18 16:27:54 +0300

    add Task 8 solution

---
+++ Homework1/Task 8/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.*/
+
+using static System.Console;
+
+Clear();
+
+Write("Enter a number: ");
+int n = Convert.ToInt32(ReadLine());
+
+int countEven = 1;
+while (countEven <= n){
+    if (countEven%2 == 0) Write ($"{countEven}, ");
+    countEven++;
+}
