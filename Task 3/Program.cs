d72a5f6d183bcb926cf2801691e699842d6bb84a
dedani4 <dedani4@gmail.com>
2022-12-18 13:52:14 +0300

    add Task 3 solution

---
+++ Task 3/Program.cs
@@ -0,0 +1,31 @@
+﻿ using static System.Console;
+ 
+ Write ("Enter number of week day:");
+ 
+ int a = Convert.ToInt32(ReadLine());
+
+if (a >0 && a<8)
+ if (a==1){
+    Write ("Monday");
+ }
+ if (a==2){
+    Write ("Tuesday");
+ }
+ if (a==3){
+    Write ("Wednesday");
+ }
+ if (a==4){
+    Write ("Thursday");
+ }
+ if (a==5){
+    Write ("Friday");
+ }
+ if (a==6){
+    Write ("Saturday");
+ }
+ if (a==7){
+    Write ("Sunday");
+ }
+
+
+
