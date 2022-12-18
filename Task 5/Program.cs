defec3e73885f4293767a780a681f58cdbdcbacf
dedani4 <dedani4@gmail.com>
2022-12-18 14:10:03 +0300

    add Task 5 solution

---
+++ Task 5/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+ 
+Write ("Enter a number:");
+ 
+int N = Convert.ToInt32(ReadLine());
+
+int list = -N;
+while (list<=N){
+    Write($"{list}, ");
+    list++;
+}
+
+
