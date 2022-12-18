3546d7f4316352461df30d702f5d805f6380048f
dedani4 <dedani4@gmail.com>
2022-12-18 13:28:43 +0300

    updated Task 1 solution

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -4,9 +4,11 @@
  WriteLine ("Enter 2 numbers: ");
  int a = Convert.ToInt32(ReadLine());
  int b = Convert.ToInt32(ReadLine());
-
- if ( a == b*b ){
-    WriteLine($"Number {a} is number {b} squared");
+ if (a>=0){
+    if ( a == b*b ){
+        WriteLine($"Number {a} is number {b} squared");
+    }
+    else 
+    WriteLine ($"Number {a} is not squared number {b}");
  }
- else 
- WriteLine ($"Number {a} is not squared number {b}");
+ else WriteLine($"Number {a} is negative");
