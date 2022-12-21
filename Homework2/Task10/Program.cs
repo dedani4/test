05db5b9217b6c694d94e557d74f6a301fae56590
dedani4 <dedani4@gmail.com>
2022-12-22 01:37:21 +0300

    add Task 10 Homework2 solution

--- Homework2/Task10/Program.cs
+++ Homework2/Task10/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.*/
+
+
+using static System.Console;
+
+Clear();
+
+Write("Enter 3-digit number: ");
+int number = Convert.ToInt32(ReadLine());
+
+while (number<100 || number>999){
+    Write("Wrong number, try again: ");
+    number = Convert.ToInt32(ReadLine());
+    }
+char[] numberArray = number.ToString().ToCharArray();
+WriteLine(numberArray[1].ToString());
+
+
