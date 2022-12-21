32ad0fd2d67dcf98160f612c0cb902585dcf7736
dedani4 <dedani4@gmail.com>
2022-12-22 02:11:35 +0300

    add Task 15 Homework2 solution

---
+++ Homework2/Task 15/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая принимает на вход цифру,
+обозначающую день недели, и проверяет, является ли этот день выходным.*/
+
+using static System.Console;
+
+Clear();
+
+Write("Enter a week day number: ");
+int number = Convert.ToInt32(ReadLine());
+
+while (number>7 || number<1){
+    Write("There are 7 days in a week, try again: ");
+    number = Convert.ToInt32(ReadLine());
+    }
+if (number == 6 || number == 7) WriteLine("This is a weekend");
+else WriteLine("This is a workday");
