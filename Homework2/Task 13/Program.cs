80c6c6ebf5096570604a9e1d5847a54de174a52b
dedani4 <dedani4@gmail.com>
2022-12-22 01:55:32 +0300

    add Task 13 Homework 2 solution

---
+++ Homework2/Task 13/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.*/
+
+using static System.Console;
+
+Clear();
+
+Write("Enter a number: ");
+int number = Convert.ToInt32(ReadLine());
+
+while (number<10){
+    Write("Missed two digits, try again: ");
+    number = Convert.ToInt32(ReadLine());
+    }
+while (number<100){
+    Write("Missed third digit, try again: ");
+    number = Convert.ToInt32(ReadLine());
+    }
+char[] numberArray = number.ToString().ToCharArray();
+WriteLine(numberArray[numberArray.Length-3].ToString());
