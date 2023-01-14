767bf952a330786509bf6f6db5c92aa344a5340b
dedani4 <dedani4@gmail.com>
2023-01-14 20:54:50 +0300

    Add Homework Task 25 27 29

---
+++ Homework 4/TAsk 27/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, реализующую метод, который принимает на вход число 
+и выдаёт сумму цифр в числе.*/
+
+using static System.Console;
+Clear();
+
+Write("Enter number: ");
+int A = Convert.ToInt32(ReadLine());
+
+WriteLine(SumDigitsNumber(A));
+
+int SumDigitsNumber(int num)
+{
+    int sum = 0;
+    for (; num > 0; num /= 10)
+    {
+        sum += num % 10;
+    }
+    return sum;
+}
