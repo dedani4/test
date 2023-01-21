986b5223c639b79183353a1e01ea2e81bd013a89
dedani4 <dedani4@gmail.com>
2023-01-21 11:06:41 +0300

    add Homework 41 & 43c

---
+++ Homework 6/Task 41/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+using static System.Console;
+Clear();
+
+Write("Enter few numbers with space: ");
+int[] array = GetArray(ReadLine());
+
+WriteLine(CountPositiveNumber(array) > 1 ?
+            $"There are {CountPositiveNumber(array)} positive numbers" :
+            CountPositiveNumber(array) > 1 ? "There is one positive number" :
+            "There is no positive numbers");
+
+
+
+int[] GetArray(string line)
+{
+    string[] lineArray = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] arr = new int[lineArray.Length];
+    for (int i = 0; i < lineArray.Length; i++)
+    {
+        arr[i] = Convert.ToInt32(lineArray[i]);
+    }
+    return arr;
+}
+
+int CountPositiveNumber(int[] arr)
+{
+    int count = 0;
+    foreach (var item in arr)
+    {
+        if (item > 0) count++;
+    }
+    return count;
+}
---
+++ Homework 6/Task 43/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0,5; -0,5)*/
+
+using static System.Console;
+Clear();
+
+Write("Enter 4 numbers with space: ");
+int[] index = GetIndex(ReadLine());
+
+WriteLine($"X = {FindCrossPoint(index).x}, Y = {FindCrossPoint(index).y}");
+
+
+
+int[] GetIndex(string line)
+{
+    string[] lineArray = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] arr = new int[lineArray.Length];
+    for (int i = 0; i < lineArray.Length; i++)
+    {
+        arr[i] = Convert.ToInt32(lineArray[i]);
+    }
+    return arr;
+}
+
+Point FindCrossPoint(int[] arr)
+{
+    Point cross = new Point();
+    cross.x = 1.0 * (arr[2] - arr[0]) / (arr[1] - arr[3]);
+    cross.y = 1.0 * arr[1] * cross.x + arr[0];
+    return cross;
+}
+
+
+
+struct Point
+{
+    public double x;
+    public double y;
+}
